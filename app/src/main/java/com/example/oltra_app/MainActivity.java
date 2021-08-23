f286bd309f879901e43b564134426bba2a9df4dc
Alfarizki Qodri <alfarizkiqodri@gmail.com>
2021-08-23 16:33:36 +0700

    <Menambah authentication firebase>

--- app/src/main/java/com/example/oltra_app/MainActivity.java
+++ app/src/main/java/com/example/oltra_app/MainActivity.java
@@ -1,27 +1,68 @@
 package com.example.oltra_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText mName,mEmail,mPassword;
     private Button btn_regist;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mName = findViewById(R.id.button_name);
+        mEmail = findViewById(R.id.button_email);
+        mPassword = findViewById(R.id.button_pass);
         btn_regist = findViewById(R.id.registerbutton);
+
+        fAuth = FirebaseAuth.getInstance();
+        //Jika udah terlogin, langsung ke homepage
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),HomePage.class));
+            finish();
+        }
 
         btn_regist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, HomePage.class);
-                startActivity(i);
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                //syarat email dan password penting
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is Required");
+                    return;
+                }
+
+                //register user in firebase
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "User Created!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomePage.class));
+                        }
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/example/oltra_app/homepagekt.kt
+++ app/src/main/java/com/example/oltra_app/homepagekt.kt
@@ -3,6 +3,7 @@ package com.example.oltra_app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomePage : AppCompatActivity() {
@@ -11,6 +12,7 @@ class HomePage : AppCompatActivity() {
         setContentView(R.layout.activity_home_page)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.fragment_community, ))
 
         bottomNavigationView.setupWithNavController(R.id)
     }
